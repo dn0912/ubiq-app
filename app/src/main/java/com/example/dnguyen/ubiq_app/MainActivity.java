aa67eaf315e317b13ae007180c553c3263357376
dn0912 <nguyen.duc0912@googlemail.com>
2018-01-07 04:06:15 +0100

    add new dialog popping up with ok and cancel button for item selection

--- app/src/main/java/com/example/dnguyen/ubiq_app/MainActivity.java
+++ app/src/main/java/com/example/dnguyen/ubiq_app/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.dnguyen.ubiq_app;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mSubscribe;
+    TextView mItemSelected;
+    String[] listItems;
+    boolean[] checkedItems;
+    ArrayList<Integer> mUserItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSubscribe = (Button) findViewById(R.id.btnSubscribe);
+        mItemSelected = (TextView) findViewById(R.id.tvItemSelected);
+
+        listItems = getResources().getStringArray(R.array.subscription_item);
+        checkedItems = new boolean[listItems.length];
+
+        mSubscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                mBuilder.setTitle("Items available for subscription");
+                mBuilder.setMultiChoiceItems(listItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int position, boolean isChecked) {
+                        if(isChecked){
+                            if(! mUserItems.contains(position)){
+                                mUserItems.add(position);
+                            } else {
+                                mUserItems.remove(position);
+                            }
+                        }
+                    }
+                });
+
+                mBuilder.setCancelable(false);
+                mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String item = "";
+                        for(int k = 0; i < mUserItems.size(); i++){
+                            item = item + listItems[mUserItems.get(k)];
+                            if(k != mUserItems.size()-1){
+                                item = item + ", ";
+                            }
+                        }
+
+                        mItemSelected.setText(item);
+                    }
+                });
+
+                mBuilder.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+            }
+        });
     }
 }
